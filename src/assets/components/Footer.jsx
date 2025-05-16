caaeaec7411a4b6f2e78033b303dfbae834ee4bb
Daniel Jorge <danyeljorge@outlook.com>
2025-05-16 13:28:13 -0300

    finalizando o footer e sua responsividade

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -11,30 +11,53 @@ const Footer = () => {
   <div className={style.conteiner0}>
     <h3>DEV.DANIEL</h3>
     <p>Transformando ideias em código com 
-      soluções front-end personalizadas.
+      soluções <br /> front-end personalizadas.
     </p>
-  </div>
-  <div className={style.conteiner}>  </div>
-  <div className={style.conteiner1}>
-    <h3>Links</h3>
+
+   <div className={style.links}>
+
     <ul>
-      <li><a href="#">Home</a></li>
+  <li><a href="https://www.instagram.com/danyeljorgee/" target='_blank'><i class="bi bi-instagram"></i></a></li>
+      <li><a href="https://www.linkedin.com/in/danieljorgee/" target='_blank'><i class="bi bi-linkedin"></i></a></li>
+      <li><a href="https://github.com/danyeljorge" target='_blank'><i class="bi bi-github"></i></a></li>
+      <li><a href="https://www.youtube.com/@trilhadacomputacao" target='_blank'><i class="bi bi-youtube"></i></a></li>
+      </ul>
+      </div>
+      
+  </div>
+  <div className={style.conteiner1}> 
+     
+     <h3>LINK ÚTEIS</h3>
+    <ul>
       <li><a href="#">Sobre Mim</a></li>
       <li><a href="#">Projetos</a></li>
-      <li><a href="#">Contato</a></li>
+      
+     
+    </ul>
+     
+     </div>
+
+
+  <div className={style.conteiner1}>
+    <h3>SUPORTE</h3>
+    <ul>
+      <li><a href="#">Contatos</a></li>
+      <li><a href="#">FAQ</a></li>
+      <li><a href="#">Whatsapp</a></li>
+      
     </ul>
   </div>
   <div className={style.conteiner1}>
-  <h3>Rede</h3>
-  <div className={style.links}>
+  <h3>FREELANCE</h3>
+  
+  <ul>
+      <li><a href="#">Skills</a></li>
+      <li><a href="#">Serviços</a></li>
+      <li><a href="#">Projeto Personalizado</a></li>
+      
+    </ul>
 
-    <ul>
-  <li><a href="#"><i class="bi bi-instagram"></i></a></li>
-      <li><a href="#"><i class="bi bi-linkedin"></i></a></li>
-      <li><a href="#"><i class="bi bi-github"></i></a></li>
-      <li><a href="#"><i class="bi bi-youtube"></i></a></li>
-      </ul>
-      </div>
+   
 
 
   </div>
@@ -42,6 +65,7 @@ const Footer = () => {
     </div>
     <div className={style.copyRight}>
       <p className={style.copy}>&copy; 2025 Dev.Daniel - Todos os Direitos Reservados.</p>
+      <a href="#" className={style.termo}>Termos de Uso</a>
       </div>
 
       <div className={style.whatsapp}>
